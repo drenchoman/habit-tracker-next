548f9a1a538a560b53dc419bcc6a477620b0ae21
Oscar Harron <oscarharron@hotmail.com>
2023-06-17 22:15:38 +1200

    add homepage title

--- src/app/page.js
+++ src/app/page.js
@@ -37,6 +37,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Habit Tracker</h1>
       <div>
         <form className="flex flex-col" onSubmit={handleForm}>
           <div className="flex flex-col my-2">
