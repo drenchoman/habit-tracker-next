defc5da9e4b7e533d17969fa942c5b5ab1062489
Oscar Harron <oscarharron@hotmail.com>
2023-06-18 21:22:01 +1200

    add getDocument firestore function

---
+++ src/app/firebase/firestore/getData.js
@@ -0,0 +1,17 @@
+import firebase_app from '../config';
+import { getFirestore, doc, getDoc } from 'firebase/firestore';
+
+const db = getFirestore(firebase_app);
+
+export default async function getDocument(collection, id) {
+  let docRef = doc(db, collection, id);
+  let result = null,
+    error = null;
+
+  try {
+    result = await getDoc(docRef);
+  } catch (e) {
+    error = e;
+  }
+  return { result, error };
+}
