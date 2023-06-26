e30f87cfee5e2a5389791eb8fa9ecef79e9fea15
Oscar Harron <oscarharron@hotmail.com>
2023-06-27 09:47:15 +1200

    add form inputs for add habit form

--- src/app/home/add/page.js
+++ src/app/home/add/page.js
@@ -19,10 +19,40 @@ export default function Admin() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-8">
       <div className="flex flex-col items-center">
+        <h2>New Habit</h2>
         <form>
-          <div>
-            <label>Habit Name</label>
-            <input type="text"></input>
+          <div className="flex flex-col my-2">
+            <label htmlFor="name">Habit Name</label>
+            <input
+              className="text-black"
+              name="name"
+              id="name"
+              type="text"
+            />
+          </div>
+          <div className="flex flex-col my-2">
+            <label htmlFor="desc">Description</label>
+            <input
+              className="text-black"
+              name="desc"
+              id="desc"
+              type="text"
+            />
+          </div>
+          <div className="flex flex-col my-2">
+            <label htmlFor="frequency">Frequency</label>
+            <input
+              placeholder="How many times a day you want to perform this habit?"
+              className="text-black"
+              name="frequency"
+              id="frequency"
+              type="number"
+            />
+          </div>
+          <div className="flex flex-col items-center my-2">
+            <button className="bg-blue-400 hover:bg-blue-700 my-2 mx-4 py-2 px-4 rounded">
+              Add
+            </button>
           </div>
         </form>
       </div>
