16ecb0184a099775f6917cf0b9db43f4dfe6a010
Oscar Harron <oscarharron@hotmail.com>
2023-07-05 22:05:14 +1200

    refactor addhabit to component

---
+++ src/app/components/addHabit.js
@@ -0,0 +1,67 @@
+'use client';
+import { useState } from 'react';
+import { useAuthContext } from '../context/AuthContext';
+import addHabit from '../firebase/firestore/addHabit';
+
+export default function AddHabit() {
+  const { user } = useAuthContext();
+  const [name, setName] = useState('');
+  const [desc, setDesc] = useState('');
+  const [freq, setFreq] = useState(1);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = {
+      name: name,
+      description: desc,
+      frequency: freq,
+    };
+
+    const { result, error } = await addHabit(user.uid, data);
+    if (error) {
+      return console.log(error);
+    }
+    console.log(result);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="flex flex-col my-2">
+        <label htmlFor="name">Habit Name</label>
+        <input
+          onChange={(e) => setName(e.target.value)}
+          className="text-black placeholder:black"
+          name="name"
+          id="name"
+          type="text"
+        />
+      </div>
+      <div className="flex flex-col my-2">
+        <label htmlFor="desc">Description</label>
+        <input
+          onChange={(e) => setDesc(e.target.value)}
+          className="text-black placeholder:black"
+          name="desc"
+          id="desc"
+          type="text"
+        />
+      </div>
+      <div className="flex flex-col my-2">
+        <label htmlFor="frequency">Frequency</label>
+        <input
+          onChange={(e) => setFreq(e.target.value)}
+          placeholder="How many times a day you want to perform this habit?"
+          className="text-black"
+          name="frequency"
+          id="frequency"
+          type="number"
+        />
+      </div>
+      <div className="flex flex-col items-center my-2">
+        <button className="bg-blue-400 hover:bg-blue-700 my-2 mx-4 py-2 px-4 rounded">
+          Add
+        </button>
+      </div>
+    </form>
+  );
+}
--- src/app/home/add/page.js
+++ src/app/home/add/page.js
@@ -1,30 +1,13 @@
 'use client';
-import Link from 'next/link';
 import React from 'react';
 import { useAuthContext } from '../../context/AuthContext';
 import { useRouter } from 'next/navigation';
-import addHabit from '@/app/firebase/firestore/addHabit';
+import AddHabit from '@/app/components/addHabit';
 import Signout from '../../components/signout';
 
 export default function Admin() {
   const { user } = useAuthContext();
   const router = useRouter();
-  const data = {
-    name: 'Test',
-    description: 'idk',
-    frequency: 2,
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    console.log(user.uid);
-    const { result, error } = await addHabit(user.uid, data);
-    if (error) {
-      return console.log(error);
-    }
-    console.log(result);
-  };
 
   React.useEffect(() => {
     if (user == null) {
@@ -38,41 +21,7 @@ export default function Admin() {
     <main className="flex min-h-screen flex-col items-center justify-center p-8">
       <div className="flex flex-col items-center">
         <h2>New Habit</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="flex flex-col my-2">
-            <label htmlFor="name">Habit Name</label>
-            <input
-              className="text-black placeholder:black"
-              name="name"
-              id="name"
-              type="text"
-            />
-          </div>
-          <div className="flex flex-col my-2">
-            <label htmlFor="desc">Description</label>
-            <input
-              className="text-black placeholder:black"
-              name="desc"
-              id="desc"
-              type="text"
-            />
-          </div>
-          <div className="flex flex-col my-2">
-            <label htmlFor="frequency">Frequency</label>
-            <input
-              placeholder="How many times a day you want to perform this habit?"
-              className="text-black"
-              name="frequency"
-              id="frequency"
-              type="number"
-            />
-          </div>
-          <div className="flex flex-col items-center my-2">
-            <button className="bg-blue-400 hover:bg-blue-700 my-2 mx-4 py-2 px-4 rounded">
-              Add
-            </button>
-          </div>
-        </form>
+        <AddHabit />
       </div>
 
       <Signout />
