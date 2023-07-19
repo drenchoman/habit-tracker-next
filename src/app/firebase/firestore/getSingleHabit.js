0f0bb98955cfeafe832e4bda63730b2be6ddd31c
Oscar Harron <oscarharron@hotmail.com>
2023-07-20 08:16:15 +1200

    add data fetching for invdividual habit page

--- src/app/firebase/firestore/getSingleHabit.js
+++ src/app/firebase/firestore/getSingleHabit.js
@@ -1,15 +1,17 @@
 import firebase_app from '../config';
 import { getFirestore, doc, getDoc } from 'firebase/firestore';
+import getDatesFromHabits from './getDatesFromHabits';
 
 const db = getFirestore(firebase_app);
 
 export default async function getSingleHabit(userid, habitid) {
-  let docRef = doc(db, users, userid, habits, habitid);
+  let docRef = doc(db, 'users', userid, 'habits', habitid);
   let result = null,
     error = null;
 
   try {
     result = await getDoc(docRef);
+    result = result.data();
   } catch (e) {
     error = e;
   }
--- src/app/home/[id]/page.js
+++ src/app/home/[id]/page.js
@@ -3,18 +3,35 @@ import { useAuthContext } from '@/app/context/AuthContext';
 import React from 'react';
 import { useRouter, useParams } from 'next/navigation';
 import getSingleHabit from '@/app/firebase/firestore/getSingleHabit';
+import getDatesFromHabits from '@/app/firebase/firestore/getDatesFromHabits';
 
 export default function HabitPage() {
   const { user } = useAuthContext();
   const { id } = useParams();
+  const [dates, setDates] = React.useState([]);
 
   React.useEffect(() => {
     const getHabitData = async () => {
       let { result, error } = await getSingleHabit(user.uid, id);
-      console.log(result, error);
+      if (error) {
+        return console.log('error', error);
+      }
+      console.log(result);
     };
-    getHabitData;
+    getHabitData();
   }, []);
+
+  React.useEffect(() => {
+    const getDates = async () => {
+      let { result, error } = await getDatesFromHabits(user.uid, id);
+      if (error) {
+        return console.log(error);
+      }
+      console.log(result);
+    };
+    getDates();
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-8">
       <div className="flex flex-col items-center"></div>
