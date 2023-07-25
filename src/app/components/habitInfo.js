781d27a9dc7e023bd31d50d724c5cbb0da2132f4
Oscar Harron <oscarharron@hotmail.com>
2023-07-26 08:38:26 +1200

    add func for total days, missed and achieved

--- src/app/components/habitInfo.js
+++ src/app/components/habitInfo.js
@@ -1,14 +1,21 @@
 'use client';
 import { FiEdit } from 'react-icons/fi';
 import Link from 'next/link';
+import { PiFireBold } from 'react-icons/Pi';
 
 export default function HabitInfo({ data, id, weeks, dates }) {
   const totalDays = dates
     .map((d) => d.length)
     .reduce((a, b) => a + b, 0);
 
-  const t = dates.map((d) => d.map((x) => x.status == true));
-  console.log(t);
+  const daysAchieved = dates
+    .map((d) =>
+      d.map((x) => x.status == true).filter((y) => y == true)
+    )
+    .map((x) => x.length)
+    .reduce((a, b) => a + b, 0);
+  // let te = t.map((x) => x.length).reduce((a, b) => a + b, 0);
+  const daysmissed = totalDays - daysAchieved;
 
   return (
     <div className="flex flex-col relative">
@@ -18,13 +25,21 @@ export default function HabitInfo({ data, id, weeks, dates }) {
         </Link>
       </div>
       <h2 className="text-2xl font-bold my-1">{data.name}</h2>
-      <p>{data.description}</p>
-      <h3>
-        Current Streak: <span>{data.currentStreak}</span>
-      </h3>
-      <p>Total Weeks: {weeks}</p>
-      <p>Total Days: {totalDays}</p>
-      <p>Frequency: {data.frequency}</p>
+      <div className="flex justify-between">
+        <p>{data.description}</p>
+        <div className="flex justify-center items-center">
+          <PiFireBold size={24} />
+          <span className="text-1xl mx-1"> {data.currentStreak}</span>
+        </div>
+      </div>
+      <hr className="my-2"></hr>
+
+      <div className="flex justify-between">
+        <p>Weeks: {weeks}</p>
+        <p>Days: {totalDays}</p>
+        <p>Achieved: {daysAchieved}</p>
+        <p>Missed: {daysmissed}</p>
+      </div>
     </div>
   );
 }
