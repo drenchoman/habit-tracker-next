584bb282b15b855904c3ffeb8ace145aac40f958
Oscar Harron <oscarharron@hotmail.com>
2023-07-28 08:30:25 +1200

    add working google font import

--- src/app/components/goAgainLetter.js
+++ src/app/components/goAgainLetter.js
@@ -1,21 +1,28 @@
 import { useState } from 'react';
 import DialogModal from './dialogModal';
+import { Monoton } from 'next/font/google';
+
+const font = Monoton({
+  weight: '400',
+  subsets: ['latin'],
+  variable: '--font-spicy',
+});
 
 export default function GoAgainLetter({ d }) {
   const [isOpened, setIsOpened] = useState(false);
   return (
-    <div className="mx-3 hover:cursor-pointer " key={d.id}>
+    <div className="mx-2 hover:cursor-pointer " key={d.id}>
       {d.status == true ? (
         <p
           onClick={() => setIsOpened(true)}
-          className="text-4xl font-spicy"
+          className={`${font.className} text-4xl `}
         >
           {d.letter}
         </p>
       ) : (
         <p
           onClick={() => setIsOpened(true)}
-          className="text-4xl opacity-50"
+          className={`${font.className} text-4xl opacity-50 `}
         >
           {d.letter}
         </p>
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,26 +1,18 @@
 'use client';
 import './globals.css';
 import { Inter } from 'next/font/google';
-import { Bungee_Spice } from 'next/font/google';
 import { AuthContextProvider } from './context/AuthContext';
-
-const inter = Inter({ subsets: ['latin'] });
-
-const bungeeFont = Bungee_Spice({
-  weight: '400',
-  subsets: ['latin'],
-  variable: '--font-spicy',
-});
 
 // export const metadata = {
 //   title: 'Habit Tracker',
 //   description: 'An application to track my habits',
 // };
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} ${bungeeFont.variable}`}>
+      <body className={inter.className}>
         <AuthContextProvider>{children}</AuthContextProvider>
       </body>
     </html>
