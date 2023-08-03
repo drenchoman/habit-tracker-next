63a09198b5cee266191dfc0ae7c1c66d9bfaff80
Oscar Harron <oscarharron@hotmail.com>
2023-08-04 08:39:18 +1200

    update forgotpassword form

--- src/app/components/forgotPassword.js
+++ src/app/components/forgotPassword.js
@@ -6,7 +6,7 @@ export default function ForgotPassword() {
   const [isOpened, setIsOpened] = useState(false);
   const [email, setEmail] = useState('');
 
-  const handleClick = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     console.log('sending reset', email);
   };
@@ -24,24 +24,26 @@ export default function ForgotPassword() {
         isOpened={isOpened}
         onClose={() => setIsOpened(false)}
       >
-        <div className="flex flex-col my-1">
-          <input
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="example@example.com"
-            className="p-2 my-1 border-4 border-black rounded-md"
-            name="email"
-            type="email"
-            id="email"
-          />
-        </div>
-        <div className="flex flex-col content-center">
-          <button
-            onClick={() => handleClick(event)}
-            className="underline underline-offset-2"
-          >
-            Send link
-          </button>
-        </div>
+        <form onSubmit={handleSubmit}>
+          <div className="flex flex-col my-1">
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="example@example.com"
+              className="p-2 my-1 border-4 border-black rounded-md"
+              name="email"
+              type="email"
+              id="email"
+            />
+          </div>
+          <div className="flex flex-col content-center">
+            <button
+              type="button"
+              className="underline underline-offset-2"
+            >
+              Send link
+            </button>
+          </div>
+        </form>
       </DialogModal>
     </div>
   );
--- src/app/components/signin.js
+++ src/app/components/signin.js
@@ -22,46 +22,48 @@ export default function SignIn() {
     router.push('/home');
   };
   return (
-    <form
-      className="p-2 border-black bg-neobackground border-4 rounded-md shadow-shadz"
-      onSubmit={handleForm}
-    >
-      <div className="flex flex-col my-1">
-        <label className="my-1" htmlFor="email">
-          Email
-        </label>
-        <input
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="example@example.com"
-          className="p-2 my-1 border-4 border-black rounded-md"
-          name="email"
-          type="email"
-          id="email"
-        />
-      </div>
-      <div className="flex flex-col my-1">
-        <label className="my-1" htmlFor="password">
-          Password
-        </label>
-        <input
-          className="p-2 my-1 border-4 border-black rounded-md"
-          onChange={(e) => setPassword(e.target.value)}
-          name="password"
-          type="password"
-          id="password"
-          autoComplete="on"
-        />
-      </div>
+    <>
+      <form
+        className="p-2 border-black bg-neobackground border-4 rounded-md shadow-shadz"
+        onSubmit={handleForm}
+      >
+        <div className="flex flex-col my-1">
+          <label className="my-1" htmlFor="email">
+            Email
+          </label>
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="example@example.com"
+            className="p-2 my-1 border-4 border-black rounded-md"
+            name="email"
+            type="email"
+            id="email"
+          />
+        </div>
+        <div className="flex flex-col my-1">
+          <label className="my-1" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="p-2 my-1 border-4 border-black rounded-md"
+            onChange={(e) => setPassword(e.target.value)}
+            name="password"
+            type="password"
+            id="password"
+            autoComplete="on"
+          />
+        </div>
+        {error ? <span className="text-center">{error}</span> : ''}
+        <div className="flex flex-col items-center">
+          <button
+            className="border-black bg-neocard border-4 rounded-md shadow-shadz my-2 mx-4 py-2 px-4 "
+            type="submit"
+          >
+            {loading ? 'Loading...' : 'Sign in'}
+          </button>
+        </div>
+      </form>
       <ForgotPassword />
-      {error ? <span className="text-center">{error}</span> : ''}
-      <div className="flex flex-col items-center">
-        <button
-          className="border-black bg-neocard border-4 rounded-md shadow-shadz my-2 mx-4 py-2 px-4 "
-          type="submit"
-        >
-          {loading ? 'Loading...' : 'Sign in'}
-        </button>
-      </div>
-    </form>
+    </>
   );
 }
