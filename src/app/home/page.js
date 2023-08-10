c2c75a527c11979da01666dbc7993acb610710a1
Oscar Harron <oscarharron@hotmail.com>
2023-08-11 08:25:02 +1200

    add spinner to home loadout

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -8,17 +8,20 @@ import Signout from '../components/signout';
 import getHabits from '../firebase/firestore/getHabits';
 import GetStarted from '../components/getStarted';
 import IncrementHabitWrapper from '../components/incrementHabitWrapper';
+import Spinner from '../components/spinner';
 
 export default function Admin() {
   const { user } = useAuthContext();
   const router = useRouter();
   const [habits, setHabits] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
 
   React.useEffect(() => {
     if (user == null) {
       router.push('/');
     } else {
       getAllHabits();
+      setLoading(false);
     }
   }, [user]);
 
@@ -30,6 +33,13 @@ export default function Admin() {
     setHabits(result);
   };
 
+  if (loading) {
+    return (
+      <main className="flex min-h-screen flex-col max-w-2xl m-auto p-4">
+        <Spinner />;
+      </main>
+    );
+  }
   return (
     <main className="flex min-h-screen flex-col max-w-2xl m-auto p-4">
       <Navbar />
