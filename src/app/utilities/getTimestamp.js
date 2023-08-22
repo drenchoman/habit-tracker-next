9e8ccadd1189cb9cac5252c2f0b5c7795b1fd040
Oscar Harron <oscarharron@hotmail.com>
2023-08-23 08:27:19 +1200

    add convert date to timestamp func

---
+++ src/app/utilities/getTimestamp.js
@@ -0,0 +1,10 @@
+// Convert my date to timestamp eg: 21092023 to 1695261722
+export default function getTimestamp(date) {
+  let string = date.toString();
+  let y = string.slice(4);
+  let m = string.slice(2, 4);
+  let d = string.slice(0, 2);
+
+  let datum = new Date(Date.UTC(y, Number(m - 1), d, 2, 2, 2));
+  return datum.getTime() / 1000;
+}
