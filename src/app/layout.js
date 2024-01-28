88859e9b56b9d8b465be98e37bdc46b1c84aa60d
Oscar Harron <oscarharron@hotmail.com>
2024-01-28 21:30:19 +1300

    update note packages + layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,25 +5,23 @@ import { Lexend_Mega } from 'next/font/google';
 import { AuthContextProvider } from './context/AuthContext';
 import { Analytics } from '@vercel/analytics/react';
 
-export async function metadata({}) {
-  return {
-    title: 'Go Again',
-    description: 'A simple habit tracker. Add. Do. Go Again.',
-    icons: [
-      {
-        rel: 'favicon',
-        href: '/favicon.ico',
-        type: 'image/ico',
-        sizes: 'any',
-      },
-      {
-        rel: 'apple-touch-icon',
-        sizes: '180x180',
-        url: '/apple-icon.png',
-      },
-    ],
-  };
-}
+export const metadata = {
+  title: 'Go Again',
+  description: 'A simple habit tracker. Add. Do. Go Again.',
+  icons: [
+    {
+      rel: 'favicon',
+      href: '/favicon.ico',
+      type: 'image/ico',
+      sizes: 'any',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      url: '/apple-icon.png',
+    },
+  ],
+};
 
 const inter = Inter({ subsets: ['latin'] });
 
